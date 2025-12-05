176d5a1b91790ce19c05fd8639d176a5ed89617c
firestarsdog <229254897+firestarsdog@users.noreply.github.com>
2025-12-04 22:49:37 -0500

    Revert "Test - ACC Dashboard Commands on Pedal Long"
    This reverts commit cfbba460dad03b082b83a0471e9ee114f68fe25f.

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -435,7 +435,8 @@ class CarController(CarControllerBase):
           can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, CanBus.POWERTRAIN, self.apply_gas, idx, acc_engaged, at_full_stop))
           can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, friction_brake_bus, self.apply_brake,
                                                                idx, CC.enabled, near_stop, at_full_stop, self.CP))
-        if self.CP.carFingerprint not in CC_ONLY_CAR or self.CP.carFingerprint == CAR.CHEVROLET_BOLT_CC:
+
+          # Send dashboard UI commands (ACC status)
           send_fcw = hud_alert == VisualAlert.fcw
           can_sends.append(gmcan.create_acc_dashboard_command(self.packer_pt, CanBus.POWERTRAIN, CC.enabled,
                                                               hud_v_cruise * CV.MS_TO_KPH, hud_control, send_fcw))
