4b7f10c84886f7f53b72c7103294ca1c52cdf911
firestarsdog <229254897+firestarsdog@users.noreply.github.com>
2026-01-12 16:01:52 -0500

    Upstream sound handling, sound check removal
    match upstream for sound handling

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -117,9 +117,6 @@ class Controls:
     self.is_metric = self.params.get_bool("IsMetric")
     self.is_ldw_enabled = self.params.get_bool("IsLdwEnabled")
 
-    # detect sound card presence and ensure successful init
-    sounds_available = HARDWARE.get_sound_card_online()
-
     car_recognized = self.CP.carName != 'mock'
 
     # cleanup old params
@@ -173,8 +170,6 @@ class Controls:
     else:
       self.startup_event = get_startup_event(car_recognized, not self.CP.passive, len(self.CP.carFw) > 0)
 
-    if not sounds_available:
-      self.events.add(EventName.soundsUnavailable, static=True)
     if not car_recognized:
       self.events.add(EventName.carUnrecognized, static=True)
       if len(self.CP.carFw) > 0:
