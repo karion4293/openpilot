a913a14b0d312b12ec19c596899a8b3a4c5b8c82
firestarsdog <229254897+firestarsdog@users.noreply.github.com>
2026-01-12 18:43:22 -0500

    Revert "match upstream for sound handling"
    This reverts commit 15775718a9d158208c612714e315488f5c143917.

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -781,6 +781,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
 
   EventName.soundsUnavailable: {
     ET.PERMANENT: NormalPermanentAlert("Speaker not found", "Reboot your Device"),
+    ET.NO_ENTRY: NoEntryAlert("Speaker not found"),
   },
 
   EventName.tooDistracted: {
